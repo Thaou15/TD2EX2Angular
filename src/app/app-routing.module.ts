0547339abc26e8cbb634ec7ad51408e307292f79
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-27 16:04:16 +0200

    dernier commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ContactComponent } from './contact/contact.component';
+import { NewsComponent } from './news/news.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RechercheComponent } from './recherche/recherche.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // new sous menu de about : dans l'url on aura : about/news
+    { path: 'about', component: AboutComponent, children :[{path: 'news', component: NewsComponent }]},
+    { path: 'accueil', component: AccueilComponent},
+    { path: 'contact', component: ContactComponent },
+    { path: 'recherche', component: RechercheComponent}, 
+    //Rediriger vers la page d'accueil
+    { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+    //Lorsque que l'on va taper un lien inconnue
+    { path: '**', component: PageNotFoundComponent }
+   
+  
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { RechercheComponent } from './recherche/recherche.component';
+import { NewsComponent } from './news/news.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    AccueilComponent,
+    RechercheComponent,
+    NewsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
